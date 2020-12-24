5c0f360171b249281804aba45240d6fca0af50a8
tanayotsaw <tanayotsaw@hotmail.com>
2020-12-24 20:25:35 +0700

    Update Fix room Entity - close #29

--- backend/ent/schema/fixroom.go
+++ backend/ent/schema/fixroom.go
@@ -1,6 +1,10 @@
 package schema
 
-import "github.com/facebookincubator/ent"
+import (
+	"github.com/facebook/ent"
+	"github.com/facebook/ent/schema/edge"
+	"github.com/facebook/ent/schema/field"
+)
 
 // FixRoom holds the schema definition for the FixRoom entity.
 //V3
@@ -10,10 +14,16 @@ type FixRoom struct {
 
 // Fields of the FixRoom.
 func (FixRoom) Fields() []ent.Field {
-	return nil
+	return []ent.Field{
+		field.String("fix_detail"),
+	}
 }
 
 // Edges of the FixRoom.
 func (FixRoom) Edges() []ent.Edge {
-	return nil
+	return []ent.Edge{
+		edge.From("customer",Customer.Type).Ref("fixs").Unique(),
+		edge.From("furnitureDetail",FurnitureDetail.Type).Ref("fixs").Unique(),
+		edge.From("room",DataRoom.Type).Ref("fixs").Unique(),
+	}
 }
